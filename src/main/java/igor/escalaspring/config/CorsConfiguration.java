93b07f1afc654e6fdccbf12cb29903ae9eaeb01f
unknown <igornascimentodesouza@gmail.com>
2022-04-07 21:33:33 -0400

    Configurando CORS

---
+++ src/main/java/igor/escalaspring/config/CorsConfiguration.java
@@ -0,0 +1,15 @@
+package igor.escalaspring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class CorsConfiguration {
+	
+	public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://127.0.0.1:5500\r\n")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+
+}
